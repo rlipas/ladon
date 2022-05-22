e4422928281b4c9dd9db3409eda4d5cbb2ba4440
Ricardo Lipas Augusto <ricardo.lipas@gmail.com>
2022-05-22 14:16:02 +0100

    Fetcher using sqlite only

--- src/ladon/fetch.py
+++ src/ladon/fetch.py
@@ -13,20 +13,6 @@ logger = logging.getLogger(__name__)
 async def fetch(provider_name, interval="1d", symbols=None):
     provider = import_module(f"ladon.providers.{provider_name}")
 
-    filename = (
-        f"{provider_name}_{interval}{'_'+'_'.join(symbols) if symbols else ''}.json.bz2"
-    )
-
-    old_klines = {}
-    try:
-        with bz2.open(filename, mode="r") as f:
-            old_info = ujson.load(f)
-            for symbol in old_info["symbols"]:
-                if "klines" in symbol and len(symbol["klines"]) > 0:
-                    old_klines[symbol["pair"]] = symbol["klines"]
-    except FileNotFoundError:
-        pass
-
     db = SqliteDatabase()
 
     info = await provider.exchange_info()
@@ -41,14 +27,16 @@ async def fetch(provider_name, interval="1d", symbols=None):
     async def fetch_until_end(symbol):
         symbol["klines"] = []
 
-        if symbol["pair"] in old_klines:
-            symbol["klines"] += old_klines[symbol["pair"]][:-1]
-            start_time = old_klines[symbol["pair"]][-1][0] // 1000
+        last_kline = db.get_candlestick(provider_name, symbol['pair'], interval)
+        if last_kline is not None:
+            start_time = last_kline[0] // 1000
         else:
             start_time = symbol["onboardDate"] // 1000
 
-        logger.info(f"Fetching {symbol['pair']} {symbol['contractType']}...")
-        symbol["klines"] += await provider.continuousKlines(
+        logger.info(
+            f"Fetching {symbol['pair']} {symbol['contractType']}, starting@{start_time}..."
+        )
+        new_klines = await provider.continuousKlines(
             symbol["pair"],
             symbol["contractType"],
             interval=interval,
@@ -61,8 +49,10 @@ async def fetch(provider_name, interval="1d", symbols=None):
             provider_name,
             symbol["pair"],
             interval,
-            {k[0] // 1000: k for k in symbol["klines"]},
+            {k[0] // 1000: k for k in new_klines},
         )
+ 
+        symbol["klines"] += new_klines
 
         if len(symbol["klines"]) == 0:
             del symbol["klines"]
@@ -70,9 +60,5 @@ async def fetch(provider_name, interval="1d", symbols=None):
         return True
 
     await asyncio.gather(*map(fetch_until_end, info["symbols"]))
-
-    with bz2.open(filename, mode="w") as f:
-        logger.info(f"Writing {filename}...")
-        f.write(ujson.dumps(info).encode())
 
     await provider.close()
